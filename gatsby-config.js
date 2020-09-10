94e118628db0b33d7b55de7deacc95880f5abed2
Adam Leskis <leskis@gmail.com>
2020-09-11 00:10:57 +0100

    updated with single page and reading json

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Merge battle: lpmi-13 vs. purcell`,
+    description: `Comparing merges into unique repos not owned by the PR author`,
+    author: `lpmi-13`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-json`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `data`,
+        path: `${__dirname}/src/data`,
       },
     },
     `gatsby-transformer-sharp`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,50 @@
 import React from "react"
-import { Link } from "gatsby"
+import { LazyImage } from 'react-lazy-images';
+import { StaticQuery, graphql } from 'gatsby';
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+const users = ['lpmi-13', 'purcell'];
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <h1>Comparing PR counts</h1>
+    <p>...only in unique repos not owned by the PR authors</p>
+    <p>(was previously a more extensive comparison, but this works for now)</p>
+
+    <div>
+      {users.map(user => (
+          <LazyImage
+              key={user}
+              alt="github avatar" 
+              src={`https://github.com/${user}.png?size=40`}
+              placeholder={({ ref }) => (
+                <img ref={ref} src={`https://github.com/github.png?size=40`} alt={"github avatar"} />
+              )}
+              actual={({ imageProps }) => <img {...imageProps} />}
+            />
+          )
+        )
+      }
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <StaticQuery
+      query={graphql`
+        query MyQuery {
+          dataJson {
+            prs {
+              author
+              date_time
+              repo
+            }
+          }
+        }
+      `}
+      render={data => (
+        data.dataJson.prs.map(item => <div key={item.repo}>author: {item.author} url: {item.repo}</div>)
+      )}
+    />
   </Layout>
 )
 
